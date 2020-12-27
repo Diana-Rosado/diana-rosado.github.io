250ae893bdbf2fb05d311a772bdf65cf71447b9a
BryanAPinos <flames539@gmail.com>
2020-12-27 16:41:39 -0500

    Scroll

--- src/components/HomePage/howitworks/howitworks.jsx
+++ src/components/HomePage/howitworks/howitworks.jsx
@@ -3,30 +3,35 @@ import { makeStyles } from "@material-ui/core";
 import phoenix_white from "./phoenix_white.png";
 import magnify from "./magnify.svg";
 import book from "./book.svg";
-import howitworksStyleSheet from './howitworksStyles.js';
+import howitworksStyleSheet from "./howitworksStyles.js";
 
 const useStyles = makeStyles(howitworksStyleSheet);
 
 function HowItWorks() {
-    const classes = useStyles();
-    
-    return(
-            <div className={classes.mission}>
-                <h1>How it Works</h1>
-                <figure className = {classes.howitworks}>
-                    <img src={phoenix_white} alt={"white phoenix"}></img>
-                    <p>Meet Serena, our assistant who will get to know you on a personal level</p>
-                </figure>
-                <figure className = {classes.howitworks}>
-                <img src={magnify} alt={""}></img>
-                    <p>We will search high and low for the right opportunities.</p>
-                </figure>
-                <figure className = {classes.howitworks}>
-                <img src={book} alt={""}></img>
-                    <p>You will then gain access to resources that are most relevant to you.</p>
-                </figure>
-            </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mission}>
+      <h1 id="about">How it Works</h1>
+      <figure className={classes.howitworks}>
+        <img src={phoenix_white} alt={"white phoenix"}></img>
+        <p>
+          Meet Serena, our assistant who will get to know you on a personal
+          level
+        </p>
+      </figure>
+      <figure className={classes.howitworks}>
+        <img src={magnify} alt={""}></img>
+        <p>We will search high and low for the right opportunities.</p>
+      </figure>
+      <figure className={classes.howitworks}>
+        <img src={book} alt={""}></img>
+        <p>
+          You will then gain access to resources that are most relevant to you.
+        </p>
+      </figure>
+    </div>
+  );
 }
 
 export default HowItWorks;
--- src/components/HomePage/mission/mission.jsx
+++ src/components/HomePage/mission/mission.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
 
-import missionStyleSheet from './missionStyles.js';
+import missionStyleSheet from "./missionStyles.js";
 
 const useStyles = makeStyles(missionStyleSheet);
 
 function Mission() {
-    const classes = useStyles();
-    
-    return(
-            <div className={classes.mission}>
-                <h1>Enhancing the educational journey</h1>
-                <p>Imagication’s mission is to provide every student  reliable resources and opportunities to enhance their educational journey.</p>
-            </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div className={classes.mission}>
+      <h1 id="mission">Enhancing the educational journey</h1>
+      <p>
+        Imagication’s mission is to provide every student reliable resources and
+        opportunities to enhance their educational journey.
+      </p>
+    </div>
+  );
 }
 
 export default Mission;
