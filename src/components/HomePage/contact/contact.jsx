078df8a270ef8336614d1ad3408ed608a6dc6ef3
Diana Rosado <drosado1215@gmail.com>
2020-12-27 00:02:36 -0500

    contact us

--- src/components/HomePage/contact/contact.jsx
+++ src/components/HomePage/contact/contact.jsx
@@ -9,8 +9,31 @@ function Contact() {
     const classes = useStyles();
     
     return(
-            <div className={classes.mission}>
-                <h1>Interested to learn more?</h1>
+            <div>
+                <div className = {classes.title}>
+                <p>Interested to learn more?</p>
+                </div>
+                <div className = {classes.contactus}>
+                    <div className ={classes.tab}>
+                        <p className = {classes.top}>
+                            We would love to hear from you
+                        </p>
+                        <p className = {classes.bottom}>
+                            Send us a message and let us know what you think
+                        </p>
+                  </div>
+                    <form>
+                    <label for = "name"></label>
+                        <input type="text" id = "name" name = "fullname" placeholder = "Your Name"/>
+
+                        <label for = "email"></label>
+                        <input type = "email" id = "email" name ="email_address" placeholder= "Your Email Address"/>
+
+                        <label for="subject"> </label>
+                        <textarea id="subject" name="subject" placeholder="Tell us what you think"></textarea>
+                        <input type = "submit" value = "Submit"/>
+                    </form>
+                </div>
             </div>
     );
 }
--- src/components/HomePage/contact/contactStyles.js
+++ src/components/HomePage/contact/contactStyles.js
@@ -1,17 +1,58 @@
 const contactStyleSheet = {
-    mission: {
-        height: '250px',
-        color: '#333399',
-        textAlign: 'center',
-        "& h1": {
-            // paddingTop: '25px',
-            fontSize: '35px',
+    contactus: {
+        borderRadius: '5px',
+        backgroundColor: '#f2f2f2',
+        padding: '20px',
+        width: '30%',
+        margin: '0 auto',
+        marginBottom: '150px',
+
+        
+        "& input, textarea": {
+            width: '80%', /* Full width */
+            padding: '12px', /* Some padding */ 
+            borderRadius: '4px', /* Rounded borders */
+            border: '2px #ccc',
+            margin: '6px 1px 16px 30px',
+            resize: 'none',
+            fontFamily: 'Sans-Serif',
+            
+        
+            
         },
-        "& p": {
-            fontSize: '25px',
-            marginRight: '350px',
-            marginLeft: '350px',
+        "& input[type=submit]": {
+            backgroundColor: '#653CBE',
+            color: 'white',
+            padding: '12px 20px',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            marginLeft: '10%',
+            
         },
+        "& input[type=submit]:hover": {
+            backgroundColor: '#5A39AE',
+          },
+    },
+    title : {
+        "& p" :{
+            color: '#653CBE',
+            fontWeight: "600",
+            fontSize: "30px",
+            textAlign: 'center',
+        },
+    },
+    top : {
+        fontWeight: "500",
+        fontSize: '24px',
+    },
+    bottom : {
+        fontSize: '14px',
+        fontWeight: '300',
+        marginTop: '-20px',
+    },
+    tab: {
+        margin:'0px 0px 30px 30px '
     },
 };
 export default contactStyleSheet;
