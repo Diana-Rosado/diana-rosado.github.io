7312659b9a39bad76977694c4ff8d21c8f21cd8f
BryanAPinos <flames539@gmail.com>
2020-12-27 21:33:10 -0500

    Mission

--- src/components/HomePage/mission/missionStyles.js
+++ src/components/HomePage/mission/missionStyles.js
@@ -1,42 +1,41 @@
 const missionStyleSheet = {
-    mission: {
-        paddingTop: '200px',
-        height: '250px',
-        color: '#333399',
-        textAlign: 'center',
-        "& h1": {
-            paddingTop: '50px',
-            fontSize: '30px',
+  mission: {
+    paddingTop: "10%",
+    height: "250px",
+    color: "#333399",
+    textAlign: "center",
+    "& h1": {
+      paddingTop: "50px",
+      fontSize: "30px",
 
-            '@media (max-width:1300px)' : {
-                fontSize: '25px',
-            },
-            
-            '@media (max-width:700px)' : {
-                fontSize: '20px',
-                },
-        },
-        "& p": {
-            fontSize: '20px',
-            width:'700px',
-            color: '#807C87',
-            margin: 'auto',
+      "@media (max-width:1300px)": {
+        fontSize: "25px",
+      },
 
-        '@media (max-width:1300px)' : {
-                fontSize: '1.1rem',
-            },
-        '@media (max-width:700px)' : {
-            fontSize: '1rem',
-            width: '500px',
-            },
-        },
+      "@media (max-width:700px)": {
+        fontSize: "20px",
+      },
     },
-    // "@media screen and (max-width: 1300px)": {
-    //     mission: {
-    //         color: 'red',
-    //         fontSize: '15px',
-    //       },
-    //     },
+    "& p": {
+      fontSize: "20px",
+      width: "700px",
+      color: "#807C87",
+      margin: "auto",
 
+      "@media (max-width:1300px)": {
+        fontSize: "1.1rem",
+      },
+      "@media (max-width:700px)": {
+        fontSize: "1rem",
+        width: "400px",
+      },
+    },
+  },
+  // "@media screen and (max-width: 1300px)": {
+  //     mission: {
+  //         color: 'red',
+  //         fontSize: '15px',
+  //       },
+  //     },
 };
 export default missionStyleSheet;
