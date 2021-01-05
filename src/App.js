7733ec5944dcbf1c047454368d2c2dc0172e0929
BryanAPinos <flames539@gmail.com>
2021-01-04 19:26:45 -0500

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import HomePage from './components/HomePage/HomePage.jsx';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
-import Form from './components/Serena/Form.jsx';
-import Interests from './components/Serena/Interests.jsx';
+import HomePage from "./components/HomePage/HomePage.jsx";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Form from "./components/Serena/Form.jsx";
+import Interests from "./components/Serena/Interests.jsx";
 
 function App() {
-  // const App = () => {
   return (
     <div>
-    <Router>
-      <Switch>
-        <Route path = "/" exact component = {HomePage}/>
-        <Route path="/Form" component = {Form}/>
-        <Route path= "/Interests" component = {Interests}/>
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/Form" component={Form} />
+          <Route path="/Interests" component={Interests} />
+        </Switch>
       </Router>
     </div>
   );
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-// import { makeStyles } from "@material-ui/core";
 import Navbar from "../Navbar/navbar.jsx";
 import Home from "./home/home.jsx";
 import Mission from "./mission/mission.jsx";
 import HowItWorks from "./howitworks/howitworks.jsx";
 import Contact from "./contact/contact.jsx";
 
-// import homePageStyleSheet from "./homePageStyles.js";
-
-// const useStyles = makeStyles(homePageStyleSheet);
-
 function HomePage() {
-  //   const classes = useStyles();
-
   return (
     <div>
       <Navbar />
