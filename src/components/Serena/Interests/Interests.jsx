67e7d5bd48cd935e368d6855ff83be39fa948de5
BryanAPinos <flames539@gmail.com>
2021-01-09 09:53:12 -0500

    Minor Edit

--- src/components/Serena/Interests/Interests.jsx
+++ src/components/Serena/Interests/Interests.jsx
@@ -1,70 +1,84 @@
-import React, {useState} from 'react';
+import React, { useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core";
 import interestStyles from "./interestStyles.js";
-import {Button} from '@material-ui/core';
-import {Link} from "react-router-dom";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import bird from "../Form/bird.svg";
-import { Multiselect, } from 'multiselect-react-dropdown';
+import { Multiselect } from "multiselect-react-dropdown";
+import { UserContext } from "../../currentData.jsx";
 
 const useStyles = makeStyles(interestStyles);
 
 function Interests() {
+  const user = useContext(UserContext);
+  const [isVisible, setVisibility] = useState(false);
+  const onClick = () => setVisibility(true);
 
-    const [isVisible,setVisibility] = useState(false)
-    const onClick = () => setVisibility(true)
+  const NextPage = () => (
+    <div className={classes.nextCtn}>
+      <Button
+        component={Link}
+        to="/resources"
+        exact
+        className={classes.nextBtn}
+      >
+        Next
+      </Button>
+    </div>
+  );
+  const searchBox = {
+    chips: {
+      background: "#663CBF",
+    },
+  };
 
-    const NextPage = () =>  (
-        <div className = {classes.nextCtn}>
-            <Button component ={Link} to = "/resources" exact className = {classes.nextBtn}>
-            Next
+  const data = [
+    { Resource: "Academic Resources", id: 1 },
+    { Resource: "Career", id: 2 },
+    { Resource: "Volunteering", id: 3 },
+    { Resource: "Scholarships", id: 4 },
+  ];
+
+  const [options] = useState(data);
+
+  const classes = useStyles();
+  return (
+    <div>
+      <nav className={classes.link}>
+        <Button className={classes.btnLink} component={Link} to="/" exact>
+          Imagication
+        </Button>
+      </nav>
+      <main className={classes.body}>
+        <figure className={classes.container}>
+          <img src={bird} alt={"Phoenix Logo"} className={classes.img} />
+        </figure>
+        <div className={classes.ctn1}>
+          <p>What are you interested in today? {user.grade} </p>
+          <form
+            className={classes.form}
+            data-toggle="buttons"
+            onClick={onClick}
+          >
+            <Multiselect
+              className={classes.resources}
+              options={options}
+              displayValue={"Resource"}
+              closeOnSelect={false}
+              avoidHighlightFirstOption={true}
+              style={searchBox}
+            />
+            {isVisible ? <NextPage /> : null}
+          </form>
+          <div className={classes.backCtn}>
+            <Button className={classes.backBtn} component={Link} to="/Form">
+              Back
             </Button>
+          </div>
         </div>
-    )
-    const searchBox = {
-        chips: {
-            background: '#663CBF'
-                },
-    }
+      </main>
+    </div>
+  );
+}
 
-    const data = [
-        {Resource: 'Academic Resources', id: 1},
-        {Resource: 'Career', id: 2},
-        {Resource: 'Volunteering', id: 3},
-        {Resource: 'Scholarships', id: 4}
-    ]
-    
-    const [options] = useState(data);
-
-    const classes = useStyles();
-    return (
-        <div>
-            <nav className = {classes.link}>
-                <Button className = {classes.btnLink} component={Link} to ="/" exact>Imagication</Button>
-            </nav>
-            <main className = {classes.body}>
-            <figure className = {classes.container}>
-                    <img src={bird} alt={"Phoenix Logo"} className = {classes.img}/>
-                </figure>
-                <div className = {classes.ctn1}>
-                    <p >What are you interested in today?</p>
-                    <form className = {classes.form} data-toggle = "buttons" onClick={onClick}>
-                        <Multiselect 
-                        className = {classes.resources} 
-                        options = {options} 
-                        displayValue ={'Resource'}
-                        closeOnSelect = {false}
-                        avoidHighlightFirstOption = {true}
-                        style={searchBox}
-                        />
-                        {isVisible ? <NextPage/> : null}
-                    </form>
-                    <div className = {classes.backCtn}>
-                        <Button className = {classes.backBtn} component = {Link} to = "/Form">Back</Button>
-                    </div>
-                </div>
-            </main>
-        </div>
-    )
-};
-
-export default Interests
+export default Interests;
--- src/components/Serena/ResourcePage/Resource.jsx
+++ src/components/Serena/ResourcePage/Resource.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core";
 import resourceStyles from "./resourceStyles.js";
+import { UserContext } from "../../currentData.jsx";
 
 const useStyles = makeStyles(resourceStyles);
 
 function Resource() {
+  const user = useContext(UserContext);
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-        <div>
-            this is the resource page
-        </div>
-    )
+  return <div>this is the resource page {user.grade}</div>;
 }
 
-export default Resource
+export default Resource;
