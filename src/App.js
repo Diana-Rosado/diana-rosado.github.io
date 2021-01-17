4297c9d496cf6c3a8b0d95e72abde7839061a6a8
Diana Rosado <drosado1215@gmail.com>
2021-01-17 14:02:17 -0500

    redo pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import HomePage from './components/HomePage/HomePage.jsx';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Form from './components/Serena/Form/Form.jsx';
 import Interests from './components/Serena/Interests/Interests.jsx';
-import Resources from './components/Serena/ResourcePage/Resource.jsx'
+import Resources from './components/Serena/ResourcePage/Resource.jsx';
+import Grade from './components/Serena/Grade/Grade.jsx';
+
 
 function App() {
   return (
     <div>
-    <Router>
-      <Switch>
-        <Route path = "/" exact component = {HomePage}/>
-        <Route path="/form" component = {Form}/>
-        <Route path= "/interests" component = {Interests}/>
-        <Route path= "/resources" component = {Resources}/>
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/form" component={Form} />
+          <Route path="/interests" component={Interests} />
+          <Route path="/resources" component={Resources} />
+          <Route path='/grade' component={Grade} />
+        </Switch>
       </Router>
     </div>
   );
--- src/components/Serena/Form/Form.jsx
+++ src/components/Serena/Form/Form.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 import formStyles from "./formStyles.js";
 import { Link } from "react-router-dom";
 import bird from "./bird.svg";
-import { Button } from "@material-ui/core";
+import { Button, Radio, FormControlLabel, FormControl, RadioGroup } from "@material-ui/core";
 import { UserContext } from "../../currentData.jsx";
 
 const useStyles = makeStyles(formStyles);
@@ -12,14 +12,18 @@ function Form() {
   const user = useContext(UserContext);
   const classes = useStyles();
 
+  // const [grade, setGrade] = useState(true);
   const [isVisible, setVisibility] = useState(false);
+  const [grade, setGrade] = useState(false);
+
   const onClick = (grade) => {
+    setGrade(false);
     setVisibility(true);
     user.setGrade(grade);
   };
 
   const NextPage = () => (
-    <div className={classes.nextCtn}>
+    <div className={classes.next}>
       <Button
         component={Link}
         to="/interests"
@@ -30,9 +34,89 @@ function Form() {
       </Button>
     </div>
   );
+
+  const pickGrade = () => {
+    setVisibility(false);
+    setGrade(true);
+  };
+
+  const GradeBtn = () => (
+    <div className={classes.next}>
+      <Button
+        component={Link}
+        to="/grade"
+        exact
+        className={classes.nextBtn}
+      >
+        Your Grade
+      </Button>
+    </div>
+  );
+  // Below is what is displayed
   return (
-    <div>
-      <nav className={classes.link}>
+    <div className={classes.background}>
+      <section className={classes.header}>
+        <img src={bird} alt={"Phoenix Logo"} className={classes.img} />
+        <nav>
+          <Button className={classes.btnLink} component={Link} to="/" exact>
+            Imagication
+          </Button>
+        </nav>
+      </section>
+      <section className={classes.group}>
+        <figure className={classes.body}>
+          <article className={classes.container}>
+            <figcaption className={classes.conText}>
+              I am in . . .
+            </figcaption>
+          </article>
+          <article>
+            <FormControl component="fieldset">
+              <RadioGroup className={classes.form} row aria-label="position" name="position" defaultValue="top">
+                <FormControlLabel
+                  className={classes.btn}
+                  value="MiddleSchool"
+                  control={<Radio color="primary" />}
+                  label={<span style={{
+                    fontWeight: 'Bold',
+                    fontSize: '1.2rem',
+                  }}>{'Middle School'}</span>}
+                  onClick={() => onClick("Middle School")}
+                  id="middleschool"
+                  name='options'
+                />{" "}
+                <FormControlLabel
+                  className={classes.btn}
+                  value="Highschool"
+                  control={<Radio color="primary" />}
+                  label={<span style={{
+                    fontWeight: 'Bold',
+                    fontSize: '1.2rem',
+                  }}>{'High School'}</span>}
+                  onClick={pickGrade}
+                  id='highschool'
+                  name='options'
+                />{" "}
+                <FormControlLabel
+                  className={classes.btn}
+                  value="College_Trade"
+                  control={<Radio color="primary" />}
+                  label={<span style={{
+                    fontWeight: 'Bold',
+                    fontSize: '1.2rem',
+                  }}>{'College or Trade'}</span>}
+                  onClick={() => onClick("College or Trade")}
+                  id='collegetrade'
+                  name='options'
+                />{" "}
+              </RadioGroup>
+            </FormControl>
+            {isVisible ? <NextPage /> : null}
+            {grade ? <GradeBtn /> : null}
+          </article>
+        </figure>
+      </section>
+      {/* <nav className={classes.link}>
         <Button className={classes.btnLink} component={Link} to="/" exact>
           Imagication
         </Button>
@@ -44,7 +128,7 @@ function Form() {
             Welcome to Imagication! Let's get to know you.
           </p>
         </figure>
-        {/* Form below */}
+        
         <div className={classes.ctn1}>
           <p>What grade are you in?</p>
           <form className={classes.form} data-toggle="buttons">
@@ -87,9 +171,51 @@ function Form() {
             {isVisible ? <NextPage /> : null}
           </form>
         </div>
-      </main>
+      </main> */}
     </div>
   );
 }
 
 export default Form;
+
+
+{/* <form className={classes.form} data-toggle="buttons">
+              <label className={classes.btn}>
+                <Radio
+                  className={classes.btnColor}
+                  color='#663CBF'
+                  checked={selectedValue === 'a'}
+                  onChange={handleChange}
+                  value="a"
+                  name="options"
+                  id="option1"
+                  onClick={() => onClick("Middle School")}
+                />{" "}
+                <span>Middle School</span>
+              </label>
+              <label className={classes.btn}>
+                <Radio
+                  className={classes.btnColor}
+                  checked={selectedValue === 'b'}
+                  onChange={handleChange}
+                  value="b"
+                  name="options"
+                  id="option2"
+                  onClick={HighSchool}   // edit here
+                />{" "}
+                <span className={classes.checkmark}>High School</span>
+              </label>
+              <label className={classes.btn}>
+                <Radio
+                  className={classes.btnColor}
+                  color='#663CBF'
+                  checked={selectedValue === 'c'}
+                  onChange={handleChange}
+                  value="c"
+                  name="options"
+                  id="option3"
+                  onClick={() => onClick("College or Trade")}
+                />{" "}
+                <span >College or Trade</span>
+              </label>
+            </form>   */}
